c0db1efe4422489f6071cfe5de0285882f444c7f
sankalesh <sankalesh.paltribe@gmail.com>
2023-02-21 19:23:15 +0530

    second commit

--- src/components/dataComponent.tsx
+++ src/components/dataComponent.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
 
-
 type DataComponentProps = {
   data: {
     items: {
@@ -10,6 +9,7 @@ type DataComponentProps = {
     }[]
   }
 }
+
 export default function DataComponent({ data }: DataComponentProps) {
 
   const [clickedItemId, setClickedItemId] = useState<string | null>(null)
@@ -22,12 +22,15 @@ export default function DataComponent({ data }: DataComponentProps) {
     <div>
       <h1 className="font-bold text-center">Accordion</h1>
       {data?.items.map((each) => (
-        <div key={each?.id} className="grid grid-cols-2 gap-4">
-         <div  className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow basis-1/2">
-         <h1 className="capitalize" onClick={() => handleOnClick(each?.id)}>{each?.name}</h1>
-          {clickedItemId === each?.id && <p>{each?.description}</p>}
-         </div>
-        </div>
+        <button
+          key={each?.id}
+          className={`grid grid-cols-2 gap-4 m-5`}
+        >
+          <div onClick={() => handleOnClick(each?.id)} className={`bg-white hover:bg-gray-100 ${clickedItemId === each?.id ? 'border border-blue-500' : ''} text-gray-800 font-semibold py-2 px-4 rounded shadow basis-1/2`}>
+            <h1 className="capitalize">{each?.name}</h1>
+            {clickedItemId === each?.id && <p className="bg-gray-300 p-[2rem]">{each?.description}</p>}
+          </div>
+        </button>
       ))}
     </div>
   )
